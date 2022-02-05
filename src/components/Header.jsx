0a920666ae7cdcc423710005217cc3753ab09635
Thiagoramos456 <thiagovirtual10@gmail.com>
2022-02-05 18:20:26 -0300

    Criando responsividade do header para desktop

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,8 +5,8 @@ import searchIcon from '../images/search.svg';
 function Header() {
   return (
     <header>
-      <nav className='flex justify-around items-center px-3 py-3'>
-        <div class='flex rounded-full border-grey-light border'>
+      <nav className='flex justify-around items-center px-1 py-3 sm:justify-end'>
+        <div class='flex rounded-full border-grey-light border w-4/6 sm:w-2/5 sm:mx-10'>
           <button>
             <span class='w-auto flex justify-end items-center text-grey p-2'>
               <img src={searchIcon} alt="Search Icon" className='w-8'></img>
@@ -18,8 +18,8 @@ function Header() {
             placeholder='Search...'
           />
         </div>
-        <div className='relative'>
-          <span className='absolute bg-black rounded-full px-1 text-sm bottom-2/4 right-2/3 sm:text-xl text-white'>
+        <div className='relative sm:mx-10'>
+          <span className='absolute bg-black rounded-full px-2 font-bold text-md bottom-2/4 right-2/3 sm:text-xl text-white'>
             5
           </span>
           <img className='w-12' alt='Cart' src={cartIcon}></img>
