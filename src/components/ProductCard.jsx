6d0f08f1fec1fb6f74f4ff44ceb1ef021284e48d
Thiagoramos456 <thiagovirtual10@gmail.com>
2022-02-10 11:27:05 -0300

    style: aumentando tamanho da imagem do card de produtos no mobile (home)

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function ProductCard({ title, price, shipment, thumbnail }) {
   return (
     <div className='px-1 pt-8 pb-4 h-18 w-1/2 shadow md:w-3/12 lg:w-4/12 lg:mx-4'>
-      <img src={thumbnail} alt='Celular' className='w-16 mx-auto lg:w-36'></img>
+      <img src={thumbnail} alt='Celular' className='w-28 mx-auto lg:w-36'></img>
       <div className='flex justify-center my-2 lg:justify-center'>
         <span className='text-sm my-1 block text-green mx-2 sm:text-xl lg:text-2xl'>{`R$${price.toFixed(2)}`}</span>
         <span className='text-xs my-1 block text-green sm:text-md lg:text-lg'>
